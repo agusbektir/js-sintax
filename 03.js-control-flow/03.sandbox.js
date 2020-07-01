aeb795ebb48815ff4deab485cde13d1eaa728005
agusbektir <agusbektir@gmail.com>
2020-07-01 20:32:58 +0700

    add while loops

--- 03.js-control-flow/03.sandbox.js
+++ 03.js-control-flow/03.sandbox.js
@@ -14,3 +14,18 @@ for(let i = 0; i < names.length; i++){
     let html = `<div>${names[i]}</div>`;
     console.log(html);
 }
+
+
+// while loops
+let q = 0;
+while(q < 5){
+    console.log('in loop = ', q);
+    q++;
+}
+
+const jenengs = ['Bapak', 'Ibuk', 'Agus Bekti', 'Dek Lisa']
+let w = 0;
+while(w < jenengs.length){
+    console.log(jenengs[w]);
+    w++;
+}
