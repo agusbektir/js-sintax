5dab40a3ca8eb14bc6633ab2fa5439201b448ee7
agusbektir <agusbektir@gmail.com>
2020-07-01 11:10:20 +0700

    initial commit

---
+++ 01.js-sintax-basic/sandbox.js
@@ -0,0 +1,29 @@
+console.log(1);
+console.log(2);
+
+let myAge = 25;
+let year = 2020;
+
+// log things to console
+console.log(myAge, year);
+// console.log(year);
+
+/*
+comment for multiline comment
+comment for multiline comment
+comment for multiline comment
+comment for multiline comment
+comment for multiline comment
+comment for multiline comment
+*/
+
+age = 30;
+console.log(age);
+
+const points = 100;
+console.log(points);
+// points = 50;
+// console.log(points);
+
+var score = 75;
+console.log(score);
